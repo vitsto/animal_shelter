93e223ee03360be2822b23c783e778ff48f37b41
vitsto <vitsto94@gmail.com>
2023-05-22 22:41:02 +1000

    начало работы с подгрузкой отчетов

--- src/main/java/pro/sky/services/impl/StorageServiceImpl.java
+++ src/main/java/pro/sky/services/impl/StorageServiceImpl.java
@@ -1,13 +1,11 @@
 package pro.sky.services.impl;
 
+import com.pengrad.telegrambot.model.File;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import pro.sky.services.StorageService;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,21 +14,23 @@ import java.util.UUID;
 @Service
 public class StorageServiceImpl implements StorageService {
     @Override
-    public String store(MultipartFile img) {
+    public String store(File img) {
         Path dir = Paths.get("/images");
-        String filename = img.getName() + UUID.randomUUID();
+        String filename = img.fileId() + UUID.randomUUID();
         Path file;
         try {
             if (!Files.exists(dir)) {
                 Files.createDirectory(dir);
             }
             file = Files.createFile(Paths.get(dir.toString(),  filename));
-            BufferedInputStream fis = new BufferedInputStream(img.getInputStream());
-            BufferedOutputStream fos = new BufferedOutputStream(new FileOutputStream(file.toFile()));
-            fos.write(fis.readAllBytes());
+
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file.toFile()));
+            oos.writeObject(img);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return filename;
     }
+
 }
